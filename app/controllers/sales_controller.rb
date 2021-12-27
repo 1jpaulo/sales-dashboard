33de31cd5b8c04f8f207cdd420e1ea4c28dd8de7
João Paulo Alvarenga <joao1310al@gmail.com>
2021-12-27 17:00:21 -0300

    Remove residual controller method

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,27 +3,6 @@ class SalesController < ApplicationController
   def index
   end
 
-  def month_sales_profit
-
-    year_one = params[:year_one].to_i
-    year_two = params[:year_two].to_i
-    month = params[:month].to_i
-
-    unless year_one == year_two - 1
-
-      render json: {errors: "Years are not consecutive."}, status: :bad_request
-
-    else
-
-      first_year_profit = Sale.year(year_one).month(month).sum(:trans_total_ex_tax)
-      second_year_profit = Sale.year(year_two).month(month).sum(:trans_total_ex_tax)
-
-      render json: {year_one: first_year_profit, year_two: second_year_profit}
-
-    end
-
-  end
-
   def month_sales_by_day
     render json: Sale.year(params[:year].to_i).month(params[:month].to_i).daily.count(:trans_total_ex_tax)
   end
